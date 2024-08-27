c20bc1e562fea9ef59dfef680ad1e38ceb9a7b27
sandeepchatgpt <168418639+sandeepchatgpt@users.noreply.github.com>
2024-08-27 19:24:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,13 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/__tests__/setup.ts'],
+    coverage: {
+      reporter: [
+        ['lcov', { 'projectRoot': './src' }],
+        ['json', { 'file': 'coverage.json' }],
+        ['text']
+      ]
+    },
   },
 })
+
